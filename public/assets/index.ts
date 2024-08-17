f41c4e52c48f1ff5511a4d633a17a7511a8ded2e
Anmeet Sekhon <113476064+AnmeetS@users.noreply.github.com>
2024-08-17 05:36:12 -0400

    updated Testimonial.tsx

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -10,5 +10,9 @@ import step1 from "./images/step1.png";
 import step2 from "./images/step2.png";
 import step3 from "./images/step3.png";
 import soc2 from "./images/soc2.png";
+import yahoo from "./images/yahoo.png";
+import matthew from "./images/matthew.jpg";
+import sully from "./images/sully.jpg";
+import royd from "./images/royd.jpg";
 
-export { algolia, hasbro, robinai, cognosys, juicebox, lenny, evalsnippet, analyticsnippet, step1, step2, step3, soc2 };
+export { algolia, hasbro, robinai, cognosys, juicebox, lenny, evalsnippet, analyticsnippet, step1, step2, step3, soc2, yahoo, matthew, sully, royd };
