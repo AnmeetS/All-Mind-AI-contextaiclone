6a940f5852667db97f19e4aaec42f1969086e5f4
Anmeet Sekhon <113476064+AnmeetS@users.noreply.github.com>
2024-08-17 12:00:14 -0400

    Update Landing.tsx

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -7,20 +7,12 @@ import GettingStarted from "./GettingStarted";
 import Enterprise from "./Enterprise";
 import Press from "./Press";
 import Testimonial from "./Testimonial";
-import { motion } from "framer-motion";
 
 const Landing = () => {
   return (
     <div className="flex text-slate-700 flex-col items-center gap-16 lg:gap-32">
       <Hero />
-      <motion.div
-        initial={{ y: 10, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-        className="w-[188%] lg:h-[80%] lgl:h-full xl:h-[200%]"
-      >
-        <TrustedBy />
-      </motion.div>
+      <TrustedBy />
 
       <Features />
       <Video />
