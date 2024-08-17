9ac98278142110362063c90448aa98b64b604310
Anmeet Sekhon <113476064+AnmeetS@users.noreply.github.com>
2024-08-17 05:03:24 -0400

    video section

--- components/Landing/Video.tsx
+++ components/Landing/Video.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <div className="flex flex-col items-center gap-4 justify-center">
+    <div className="flex flex-col items-center gap-4 rounded-md overflow-hidden max-w-4xl">
+      <video controls>
+        <source src="https://blob.context.ai/2024MayAnalyticswBackground.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+
+    </div>
+  </div>
   )
 }
 
